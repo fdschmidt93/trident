7fdc297cd18a9ba04deae5546818c3938f9cc3c2
Fabian David Schmidt <fabian.david.schmidt@gmail.com>
2023-10-18 10:29:53 +0200

    feat(tests): add many explict eval config tests

--- tests/test_experiments.py
+++ tests/test_experiments.py
@@ -26,24 +26,47 @@ def test_multi_train_dataloader() -> None:
         assert torch.allclose(torch.zeros(1)[0], torch.Tensor(train(cfg)))
 
 
-def test_off_by_one() -> None:
+def test_off_by_one_test_single_train_many_val_test_single_eval_config() -> None:
     with initialize(
         version_base=None,
         config_path=CONFIG_PATH,
     ):
         # config is relative to a module
-        cfg = compose(config_name="test_single_train_many_val_test")
+        cfg = compose(config_name="test_single_train_many_val_test_single_eval_config")
         assert torch.allclose(torch.Tensor([1]), torch.Tensor(train(cfg)))
 
 
-def test_off_by_two() -> None:
+def test_off_by_two_test_single_train_many_val_test_single_eval_config() -> None:
     with initialize(
         version_base=None,
         config_path=CONFIG_PATH,
     ):
         # config is relative to a module
         cfg = compose(
-            config_name="test_single_train_many_val_test",
+            config_name="test_single_train_many_val_test_single_eval_config",
+            overrides=["optimized_metric='off_by_two/val/mse_loss'"],
+        )
+        assert torch.allclose(torch.Tensor([4]), torch.Tensor(train(cfg)))
+
+
+def test_off_by_one_test_single_train_many_val_test_single_eval_configs() -> None:
+    with initialize(
+        version_base=None,
+        config_path=CONFIG_PATH,
+    ):
+        # config is relative to a module
+        cfg = compose(config_name="test_single_train_many_val_test_many_eval_configs")
+        assert torch.allclose(torch.Tensor([1]), torch.Tensor(train(cfg)))
+
+
+def test_off_by_two_test_single_train_many_val_test_many_eval_config() -> None:
+    with initialize(
+        version_base=None,
+        config_path=CONFIG_PATH,
+    ):
+        # config is relative to a module
+        cfg = compose(
+            config_name="test_single_train_many_val_test_many_eval_configs",
             overrides=["optimized_metric='off_by_two/val/mse_loss'"],
         )
         assert torch.allclose(torch.Tensor([4]), torch.Tensor(train(cfg)))
